05f6da126d3b7b4571cbcc3453690c694b1573c8
Muhammad Nur Ashiddiqi <43430326+Tnembull@users.noreply.github.com>
2020-11-15 23:26:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<html>
+    <head>
+        <title>Page Not Found</title>
+        <style>
+            body{
+                margin:0;
+                padding:30px;
+                font:12px/1.5 Helvetica,Arial,Verdana,sans-serif;
+            }
+            h1{
+                margin:0;
+                font-size:48px;
+                font-weight:normal;
+                line-height:48px;
+            }
+            strong{
+                display:inline-block;
+                width:65px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Halaman Tidak Ditemukan</h1>
+        <p>
+            
+            Kamu mau cari apa dihalaman ini :)
+            
+        </p>
+        <a href='https://tnembull.github.io/Batiqu/index.php/'>Kunjungi Disini</a>
+    </body>
+</html>
